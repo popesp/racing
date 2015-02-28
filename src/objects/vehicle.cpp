f6c454a2665a39cd1ed0f66e1c05730719c38231
Kyle Kajorinne <kylekajorinne@gmail.com>
2015-02-27 19:37:33 -0700

    back button update

--- src/objects/vehicle.cpp
+++ src/objects/vehicle.cpp
@@ -222,7 +222,10 @@ static void vehicleinput(struct vehiclemanager* vm, struct vehicle* v, float spe
 			vec3f_negate(force);
 		
 		physx::PxRigidBodyExt::addLocalForceAtLocalPos(*v->body, physx::PxVec3(force[VX], force[VY], force[VZ]), physx::PxVec3(0.f, 0.f, -vm->dim[VZ]/2.f));
-
+		
+		if (v->controller->buttons[INPUT_BUTTON_BACK] == (INPUT_STATE_CHANGED | INPUT_STATE_DOWN)){
+			vehicle_reset(vm, v);
+		}
 		// firing a projectile
 		if (v->controller->buttons[INPUT_BUTTON_A] == (INPUT_STATE_DOWN | INPUT_STATE_CHANGED))
 		{
@@ -314,9 +317,6 @@ void vehiclemanager_update(struct vehiclemanager* vm)
 
 		physx::PxRigidBodyExt::addLocalForceAtLocalPos(*v->body, physx::PxVec3(force[VX], force[VY], force[VZ]), physx::PxVec3(0.f, 0.f, 0.f));
 
-		if (vm->vehicles->controller[0].buttons[INPUT_BUTTON_BACK] == (INPUT_STATE_CHANGED | INPUT_STATE_DOWN)){
-			vehicle_reset(vm, v);
-		}
 	}
 }
 
