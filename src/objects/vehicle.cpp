5e4dc52b16948c1a4b380eff356c3d8503c477b9
Sam Evans <smlevans@gmail.com>
2015-03-09 20:58:55 -0600

    Update vehicle.cpp

--- src/objects/vehicle.cpp
+++ src/objects/vehicle.cpp
@@ -49,9 +49,6 @@ static void vehicleinput(struct vehiclemanager* vm, struct vehicle* v, float spe
 			vec3f_negate(force);
 		
 		physx::PxRigidBodyExt::addLocalForceAtLocalPos(*v->body, physx::PxVec3(force[VX], force[VY], force[VZ]), physx::PxVec3(0.f, 0.f, -vm->dim[VZ]/2.f));
-	//	if(v->owns!=NULL){
-	//		physx::PxRigidBodyExt::addForceAtLocalPos(*v->owns->body,physx::PxVec3(force[VX], force[VY], force[VZ]), physx::PxVec3(0.f, 0.f, -vm->dim[VZ]/2.f));
-	//	}
 
 		// reset button
 		if (v->controller->buttons[INPUT_BUTTON_BACK] == (INPUT_STATE_CHANGED | INPUT_STATE_DOWN)){
@@ -109,9 +106,9 @@ static void vehicleinput(struct vehiclemanager* vm, struct vehicle* v, float spe
 
 		//spawn blimp
 		if (v->controller->buttons[INPUT_BUTTON_X] == (INPUT_STATE_DOWN | INPUT_STATE_CHANGED)){
-			vec3f test;
-			vec3f_set(test, -22.f, 10.f,-115.f);
-			entitymanager_lapblimp(vm->em,test);
+			//vec3f test;
+			//vec3f_set(test, -22.f, 10.f,-115.f);
+			//entitymanager_lapblimp(vm->em,test);
 		}
 	}
 }
